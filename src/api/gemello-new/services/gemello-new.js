fc535b1b82e14d973b082ca35793a772035c06ef
Dpaace <dipeshnepali767@gmail.com>
2024-09-10 14:16:19 +0545

    Trying custom API

--- src/api/gemello-new/services/gemello-new.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * gemello-new service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::gemello-new.gemello-new');
